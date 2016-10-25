4c97f03c4d2a79638b646b0b5c06d9f8fe67b77f
Rishabh Jain <mailrishabh1403@gmail.com>
2016-10-25 21:35:29 +0530

    all types

--- background.js
+++ background.js
@@ -4,23 +4,51 @@ var contextTypes = [
         "image",
         "link"
     ];
-for(var i=0;i<5;i++){
+for(var i=0;i<4;i++){
+    var c = contextTypes[i];
     var contextMenuOptions = {
-    "id" : contextTypes[i],
-    "title" : "Share On %s Twitter",
-    "contexts" : contextTypes[i];
-}
+        "id" : c,
+        "title" : "Share %s On Twitter",
+        "contexts" : [c]
+        }
+    chrome.contextMenus.create(contextMenuOptions);
 }
 
 
-chrome.contextMenus.create(contextMenuOptions);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
-    alert(clickData.menuItemId);
-    var tabObject = {
+    //alert(clickData.menuItemId);
+    if(clickData.menuItemId==="selection"){
+        var tabObject = {
         "url" : "https://twitter.com/intent/tweet?text="+clickData.selectionText
+        }
+        chrome.tabs.create(tabObject,function(tab){
+             alert(tab.id);
+         })
     }
-    chrome.tabs.create(tabObject,function(tab){
-        alert(tab.id);
-    })
+    else if(clickData.menuItemId==="image"){
+        var tabObject = {
+        "url" : "https://twitter.com/intent/tweet?text="+clickData.selectionText
+        }
+        chrome.tabs.create(tabObject,function(tab){
+             alert(tab.id);
+         })
+    } 
+    else if(clickData.menuItemId==="link"){
+        var tabObject = {
+        "url" : "https://twitter.com/intent/tweet?url="+clickData.linkUrl
+        }
+        chrome.tabs.create(tabObject,function(tab){
+             alert(tab.id);
+         })
+    }
+    else if(clickData.menuItemId==="page"){
+        var tabObject = {
+        "url" : "https://twitter.com/intent/tweet?text="+clickData.selectionText
+        }
+        chrome.tabs.create(tabObject,function(tab){
+             alert(tab.id);
+         })
+    }
+    
 })
